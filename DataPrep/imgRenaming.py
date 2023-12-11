2a052fd126b7159e6d61d7b29f45733689bbbbdf
Sid <siddiqmoideen07@gmail.com>
2023-12-11 12:51:34 +0530

    Fed imgReNaming Code

---
+++ DataPrep/imgRenaming.py
@@ -0,0 +1,21 @@
+from tkinter import filedialog
+import random
+import tqdm
+import glob
+import time
+import os
+
+t1 = time.time()
+print("Select the Directory Yo!")
+imgDir = filedialog.askdirectory() + '\\'
+print("---->", imgDir)
+x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
+x = sum(x , [])
+random.shuffle(x)
+
+a=0
+for i in tqdm.tqdm(x, desc = "ReNamin' the Images Yo!", colour = 'red'):
+    os.rename(i, imgDir+str(a)+'.png')
+    a+=1
+t2 = time.time()
+print("\nCompleteExecTime: ", (t2-t1))

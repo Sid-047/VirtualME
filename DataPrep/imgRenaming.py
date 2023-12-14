fc4d8c36cee1c623317a548545669755fb98a8fd
Sid <siddiqmoideen07@gmail.com>
2023-12-14 16:36:58 +0530

    ReNamed dataPrep Objects

--- DataPrep/imgRenaming.py
+++ DataPrep/imgRenaming.py
@@ -7,7 +7,7 @@ import os
 
 t1 = time.time()
 print("Select the Directory Yo!")
-imgDir = filedialog.askdirectory() + '\\'
+imgDir = filedialog.askdirectory() + '//'
 print("---->", imgDir)
 x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
 x = sum(x , [])
@@ -15,7 +15,7 @@ random.shuffle(x)
 
 a=0
 for i in tqdm.tqdm(x, desc = "ReNamin' the Images Yo!", colour = 'red'):
-    os.rename(i, imgDir+str(a)+'.png')
+    os.rename(i, imgDir+str(a)+'.'+i.split('.')[-1])
     a+=1
 t2 = time.time()
 print("\nCompleteExecTime: ", (t2-t1))

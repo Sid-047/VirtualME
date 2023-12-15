1d90fcee0b109ec6bceb9a1753123a4529562509
Sid <siddiqmoideen07@gmail.com>
2023-12-15 14:11:25 +0530

    Refined Code

--- DataPrep/imgCaptioning.py
+++ DataPrep/imgCaptioning.py
@@ -1,6 +1,6 @@
-from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from tkinter import filedialog
+from PIL import Image
 import random
 import torch
 import tqdm
@@ -10,7 +10,7 @@ import os
 
 t1 = time.time()
 print("Select the Directory Yo!")
-imgDir = filedialog.askdirectory() + '\\'
+imgDir = filedialog.askdirectory() + '//'
 print("---->", imgDir)
 x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
 x = sum(x , [])
@@ -22,7 +22,7 @@ for i in tqdm.tqdm(x, desc = "captionin' the Images Yo!", colour = 'red'):
     rawImg = Image.open(i).convert('RGB')
     inputs = processor(rawImg, return_tensors="pt").to(device)
     out = model.generate(**inputs)
-    print(processor.decode(out[0], skip_special_tokens=True).replace('here is a', 'a'))
+    print(processor.decode(out[0], skip_special_tokens=True).replace('there is a', 'a'))
 
 t2 = time.time()
 print("\nCompleteExecTime: ", (t2-t1))

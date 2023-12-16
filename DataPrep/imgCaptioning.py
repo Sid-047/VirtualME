e555ac1ae7b9863aeb14a96848adca79e3947845
Sid <siddiqmoideen07@gmail.com>
2023-12-16 14:54:28 +0530

    Initiated TxTfile Stuff

--- DataPrep/imgCaptioning.py
+++ DataPrep/imgCaptioning.py
@@ -8,6 +8,10 @@ import glob
 import time
 import os
 
+print("\n\nFed the Stuff... Jus' Hit Enter if there's None")
+preCon = input("PreCaption Contents Yo: ") + ' '
+postCon = input("PostCaption Contents Yo: ")
+
 t1 = time.time()
 print("Select the Directory Yo!")
 imgDir = filedialog.askdirectory() + '//'
@@ -15,9 +19,6 @@ print("---->", imgDir)
 x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
 x = sum(x , [])
 
-print("\n\nFed the Stuff... Jus' Hit Enter if there's None")
-preCon = input("PreCaption Contents Yo: ")
-postCon = input("PostCaption Contents Yo: ")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to(device)
@@ -28,7 +29,10 @@ for i in tqdm.tqdm(x, desc = "Captionin' the Images Yo!", colour = 'red'):
     out = model.generate(**inputs)
     genOut = processor.decode(out[0], skip_special_tokens=True).replace('there is a', 'a')
     txtOut = preCon + genOut + postCon
-    print(txtCon)
+    print(txtOut)
+    f = open(i.split('.')[-2] + '.txt', 'w')
+    f.write(txtOut)
+    f.close()
 
 t2 = time.time()
 print("\nCompleteExecTime: ", (t2-t1))

14e02598f0e729cc658789bf076814903199e859
sid <sid@adoltech.com>
2024-01-15 19:45:46 +0530

    Integrated LoRA Stuff

--- imgGen/myStuff/singleST/magic.py
+++ imgGen/myStuff/singleST/magic.py
@@ -7,9 +7,7 @@ import time
 import os
 
 print(Fore.BLUE+Style.BRIGHT+"\n\nSelect the fineTuned SafeTensor File Yo!"+Fore.RESET)
-inTxt = filedialog.askopenfilename(filetypes=[("LoRA SafeTensor", "*.safetensors")])
-print(Fore.CYAN+Style.BRIGHT+"\n\nand Come On! Select OUT Directory"+Fore.RESET)
-outDir = filedialog.askdirectory() + '\\'
+inLoRA = filedialog.askopenfilename(filetypes=[("LoRA SafeTensor", "*.safetensors")])
 
 w = 768
 h = 768 
@@ -27,16 +25,16 @@ nPrompt = "low quality, ugly, tiling, poorly drawn hands, poorly drawn feet, \
 gSeed = torch.Generator(device="cuda").manual_seed(valSeed)
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe = pipe.to("cuda")
+pipe.load_lora_weights(inLoRA)
+pipe.fuse_lora()
 
 t1 = time.time()
-
 img = pipe(prompt = pPrompt, negative_prompt = nPrompt, height = h, width = w,
            num_inference_steps = nSampling, generator = gSeed, guidance_scale = gScale).images[0]
 
 if not os.path.isdir("genImg"):
    os.makedirs("genImg")
 img.save("genImg\{}.png".format(time.time()))
-
 t2 = time.time()
 
 print("\nExecTime: ", (t2-t1))
